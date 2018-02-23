f425d628874703099f12ee78e66d36a3ee987f85
MEGANET\Brett.Hamm <brett.hamm@megasys.com>
2018-02-23 16:58:50 -0700

    Further changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyBVFNqBQBqbCUBoY5B57ZA_tUNDj7o1Sw0";
 
-// Create new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] }; 
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({ videos: videos});
+        });
+    }
+
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+                
+            </div>
+        );
+    }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
 
